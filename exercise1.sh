e6af7ecc417a553f759a101e9512123f3d0b78ea
Penghao Xu <52328364+xph9876@users.noreply.github.com>
2021-03-25 13:59:59 -0400

    Add files via upload

---
+++ exercise1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Hello world!"
+echo "Hello world!"
+echo "Hello world!"
+echo "Hello world!"
+echo "Hello world!"
+echo "Hello world!"
+echo "Hello world!"
+echo "Hello world!"
+echo "Hello world!"
+echo "Hello world!"
+
+sleep 5s
+
+# run from lab23
+ls -a ~ | wc -l > exercise1.txt
+
+# run from home
+# ls -a | wc -l > lab23/exercise1.txt
+
